7994e6c6b3f8105d3099be5c05054683e84a1cf2
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-08-29 13:42:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//app.js
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+
+// INITIALIZE THE GIPHY-API LIBRARY
+var giphy = require('giphy-api')();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: res.data})
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
+});
